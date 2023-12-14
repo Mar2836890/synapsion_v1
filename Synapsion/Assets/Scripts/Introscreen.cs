34167e3775e050ed01e64ab0d92851d8dfcfed9b
Marjolein van Tol <m.van.tol.2@umail.leidenuniv.nl>
2023-12-14 17:56:53 +0100

    Simple start screen

---
+++ Synapsion/Assets/Scripts/Introscreen.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroPopup : MonoBehaviour
+{
+    // Reference to the button inside the pop-up
+    public Button closeButton;
+
+    private void Start()
+    {
+        // Attach a listener to the button's onClick event
+        closeButton.onClick.AddListener(ClosePopup);
+    }
+
+    // Method to close the pop-up
+    private void ClosePopup()
+    {
+        // Deactivate or destroy the pop-up GameObject
+        gameObject.SetActive(false); // Alternatively, you can use Destroy(gameObject);
+    }
+}
