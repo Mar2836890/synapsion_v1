96022fba003efb9c9d3884503723706c6b0c5e03
Marjolein van Tol <m.van.tol.2@umail.leidenuniv.nl>
2023-12-20 12:32:16 +0100

    inf button works

--- Synapsion/Assets/Scripts/UI/Introscreen.cs
+++ Synapsion/Assets/Scripts/UI/Introscreen.cs
@@ -5,11 +5,18 @@ public class IntroPopup : MonoBehaviour
 {
     // Reference to the button inside the pop-up
     public Button closeButton;
+    public Button openButton;
 
     private void Start()
     {
         // Attach a listener to the button's onClick event
         closeButton.onClick.AddListener(ClosePopup);
+        openButton.onClick.AddListener(OpenPopup);
+    }   
+
+    private void OpenPopup()
+    {
+        gameObject.SetActive(true); 
     }
 
     // Method to close the pop-up
